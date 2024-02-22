0e0ee26c06f31fa5b5c4f70e678b759d0502c798
Djeb94 <151559648+Djeb94@users.noreply.github.com>
2024-02-22 18:04:23 +0100

    email interface

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
-    <h1>{{ message }}</h1>
+  <h1 v-if="user === null">{{ message }}</h1>
+  <div v-else>
+    <form action="ConversationsPage.vue" method="post">
+      <label for="to">À :</label>
+      <input type="email" id="to" name="to" required placeholder="Destinataire">
+
+      <label for="subject">Sujet :</label>
+      <input type="text" id="subject" name="subject" required placeholder="Objet"  >
+
+      <label for="message">Message :</label>
+      <textarea id="message" name="message" rows="5" required placeholder="Votre message"></textarea>
+
+      <button id="send">Envoyer</button>
+    </form>
   </div>
 </template>
-  
+
 <script>
+import * as microsoftGraph from "../lib/microsoftGraph.js";
+import { mapMutations, mapState } from 'vuex'
 
 export default {
   name: "HomePage",
@@ -12,13 +26,68 @@ export default {
     return {
       message: 'Welcome to Email Manager'
     }
-  }
+  },
+  computed: {
+    ...mapState(['user'])
+  },
+  methods: {
+    ...mapMutations(['setUser']),
+    async signIn() {
+      try {
+        const user = await microsoftGraph.signInAndGetUser();
+        this.setUser(user.account.name);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async signOut() {
+      try {
+        await microsoftGraph.signOutUser();
+        this.setUser(null);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
 }
-
 </script>
 
 <style>
   div h1 {
     margin-top: 100px;
+  
+  }
+  form {
+    margin: 0 auto;
+      max-width: 550px;
+    margin-top: 80px;
+  }
+  label, input, textarea {
+   display: flex;
+    margin-bottom: 10px;
+    border-radius: 20px;
+    width: 500px;
+    padding: 5px;
+    padding-left: 10px;
+  }
+
+  textarea{
+    height: 150px;
+    padding: 10px;
+  }
+
+  #send {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    border-radius: 20px;
+    float: right;
+    margin-right: 35px;
+    margin-top: 20px;
+  }
+  #send:hover {
+    background-color: #0056b3;
   }
 </style>
