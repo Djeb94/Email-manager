5bfa0eea80e7d78b12b007c9628af3f06c53ba12
Maxime <maximeye1234@gmail.com>
2024-02-22 18:03:45 +0100

    fix route

--- src/route.js
+++ src/route.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './pages/HomePage.vue'
 import IndexPage from './pages/ConversationsIndexPage.vue'
 import ConversationContent from './pages/ConversationsContent.vue'
+import store from './store'
 
 const routes = [
   { path: '/', component: HomePage },
@@ -19,6 +20,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.isAuthenticated) {
+    next('/') // Redirige vers la page d'accueil si l'utilisateur n'est pas authentifié
+  } else {
+    next() // Sinon, autorise l'accès à la route
+  }
 })
 
 export default router;
