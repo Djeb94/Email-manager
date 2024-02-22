83450a333ad6220d6fd980a5e5d735a732eeb6ff
Maxime <maximeye1234@gmail.com>
2024-02-22 19:13:37 +0100

    fix

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,14 +2,13 @@
   <div>
     <h1>{{ welcome }}</h1>
     <p v-if="!isAuthenticated">{{ auth }}</p>
-    <h1 v-if="!isAuthenticated">{{ welcome }}</h1>
     <div v-else>
       <form action="ConversationsPage.vue" method="post">
         <label for="to">À :</label>
         <input type="email" id="to" name="to" required placeholder="Destinataire">
 
         <label for="subject">Sujet :</label>
-        <input type="text" id="subject" name="subject" required placeholder="Objet"  >
+        <input type="text" id="subject" name="subject" required placeholder="Objet">
 
         <label for="message">Message :</label>
         <textarea id="message" name="message" rows="5" required placeholder="Votre message"></textarea>
@@ -39,41 +38,45 @@ export default {
 </script>
 
 <style>
-  div h1 {
-    margin-top: 100px;
-  }
-  form {
-    margin: 0 auto;
-    max-width: 550px;
-    margin-top: 80px;
-  }
-  label, input, textarea {
-    display: flex;
-    margin-bottom: 10px;
-    border-radius: 20px;
-    width: 500px;
-    padding: 5px;
-    padding-left: 10px;
-  }
+div h1 {
+  margin-top: 100px;
+}
 
-  textarea{
-    height: 150px;
-    padding: 10px;
-  }
+form {
+  margin: 0 auto;
+  max-width: 550px;
+  margin-top: 80px;
+}
 
-  #send {
-    padding: 10px 20px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-    border-radius: 20px;
-    float: right;
-    margin-right: 35px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-  }
-  #send:hover {
-    background-color: #0056b3;
-  }
-</style>
+label,
+input,
+textarea {
+  display: flex;
+  margin-bottom: 10px;
+  border-radius: 20px;
+  width: 500px;
+  padding: 5px;
+  padding-left: 10px;
+}
+
+textarea {
+  height: 150px;
+  padding: 10px;
+}
+
+#send {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  border-radius: 20px;
+  float: right;
+  margin-right: 35px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+#send:hover {
+  background-color: #0056b3;
+}</style>
