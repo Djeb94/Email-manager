02e40438ec703a4d861086953e11c1eb1c658ec7
Maxime <maximeye1234@gmail.com>
2024-02-23 16:49:20 +0100

    Login Google, récupérer userInfo (nom utilisateur) non fonctionnel pour google

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,61 @@
 <template>
   <div class="auth-container">
-    <button v-if="user === null" @click="signIn" class="auth-button microsoft">Sign in with Microsoft</button>
-    <button v-if="user === null" @click="signInG" class="auth-button google">Sign in with Google</button>
-    <button v-else @click="signOut" class="auth-button sign-out">Sign out</button>
-    <p v-if="user !== null" class="welcome-text">{{ user }}</p>
+    <button v-if="!isLoggedIn" @click="signInMicrosoft" class="auth-button microsoft">Sign in with Microsoft</button>
+    <button v-if="!isLoggedIn" class="auth-button google">
+      <GoogleLogin :callback="callbackGoogle">Sign in with Google</GoogleLogin>
+    </button>
+    <button v-if="isLoggedIn" @click="signOut" class="auth-button sign-out">Sign out</button>
+    <p v-if="isLoggedIn" class="welcome-text">{{ userInfo }}</p>
   </div>
 </template>
 
 <script>
 import * as microsoftGraph from "../lib/microsoftGraph.js";
 import { mapMutations, mapState } from 'vuex'
+import { googleLogout } from "vue3-google-login";
 
 export default {
-  name: "SigninButton",
+  name: "LogIn",
+  data() {
+    return {
+      isLoggedIn: false,
+      userInfo: null,
+    }
+  },
   computed: {
     ...mapState(['user'])
   },
   methods: {
     ...mapMutations(['setUser']),
-    async signIn() {
+    callbackGoogle(response) {
+      console.log('Logged in with Google')
+      this.isLoggedIn = true
+      console.log(response)
+      this.userInfo = response.profileObj && response.profileObj.name ? response.profileObj.name : 
+                     response.name ? response.name :
+                     response.profileObj && response.profileObj.email ? response.profileObj.email : 
+                     "Unknown User";
+      this.setUser(this.userInfo);
+    },
+    async signInMicrosoft() {
       try {
         const user = await microsoftGraph.signInAndGetUser();
-        this.setUser(user.account.name);
+        this.isLoggedIn = true;
+        this.userInfo = user.account.name; // Récupération du nom de l'utilisateur Microsoft
+        this.setUser(this.userInfo);
       } catch (error) {
         console.error(error);
       }
     },
     async signOut() {
       try {
-        await microsoftGraph.signOutUser();
-        this.setUser(null);
+        if (this.isLoggedIn) {
+          await microsoftGraph.signOutUser();
+        } else {
+          googleLogout();
+        }
+        this.isLoggedIn = false;
+        this.$store.commit('clearUser');
       } catch (error) {
         console.error(error);
       }
--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './route'
 import store from './store'
+import vue3GoogleLogin from 'vue3-google-login'
 
-createApp(App).use(router).use(store).mount('#app')
+const CLIENT_ID= "93739110920-ojj41p4nookibc1urvt1a582nq55no7u.apps.googleusercontent.com"
+
+createApp(App)
+    .use(vue3GoogleLogin, {
+        clientId: CLIENT_ID
+    })
+    .use(router)
+    .use(store)
+    .mount('#app')
